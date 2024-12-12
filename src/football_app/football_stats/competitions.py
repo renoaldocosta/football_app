f6a954cc8a7a44e673b64e7ef1a5d33f3b61b920
renoaldo <renoaldo@hotmail.com>
2024-12-12 17:33:11 -0300

    Criação da função get_matches no arquivo footbal_stats/competitions.py

---
+++ src/football_app/football_stats/competitions.py
@@ -0,0 +1,8 @@
+from statsbombpy import sb
+import json
+
+# obtém os dados das partidas de uma competição e temporada específicas.
+def get_matches(competition_id: int, season_id: int) -> str:
+    return json.dumps(
+        sb.matches(competition_id=competition_id, season_id=season_id).to_dict(orient='records')
+    )
