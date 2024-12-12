84fba2ccdc7de3c10432184722d8fe5f665c0eb3
renoaldo <renoaldo@hotmail.com>
2024-12-12 17:43:09 -0300

    Integração do LLMChain e análise do jogo

--- src/football_app/tools/football.py
+++ src/football_app/tools/football.py
@@ -1,5 +1,12 @@
 from statsbombpy import sb  # type: ignore
 import json
+from langchain_google_genai import GoogleGenerativeAI
+import yaml
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_match_details_match_id(match_id: int) -> dict:
     """
@@ -42,6 +49,55 @@ def filter_starting_xi(line_ups: str) -> dict:
                 continue
     return filter_starting_xi
 
+
+# Gera comentários especializados sobre a partida, utilizando LLMChain com o modelo GoogleGenerativeAI
+def get_sport_specialist_comments_about_match(match_details: str, line_ups: str) -> str:
+    """
+    Returns the comments of a sports specialist about a specific match.
+    The comments are generated based on match details and lineups.
+    """
+    
+    line_ups = filter_starting_xi(line_ups)
+    
+    agent_prompt = """
+    You are a sports commentator with expertise in football (soccer). Respond as
+    if you are delivering an engaging analysis for a TV audience. Here is the
+    information to include:
+
+    Instructions:
+    1. Game Overview:
+        - Describe the importance of the game (league match, knockout, rivalry, etc.).
+        - Specify when and where the game took place.
+        - Provide the final result.
+    3. Analysis of the Starting XI:
+        - Evaluate the starting lineups for both teams.
+        - Highlight key players and their roles.
+        - Mention any surprising decisions or notable absences.
+    3.  Contextual Insights:
+        - Explain the broader implications of the match (rivalry, league standings, or storylines).
+    4. Engaging Delivery:
+        - Use a lively, professional, and insightful tone, making the commentary
+        appealing to fans of all knowledge levels.
+    
+    The match details are provided by the provided as follow: 
+    {match_details}
+    
+    The team lineups are provided here:
+    {lineups}
+    
+    Provide the expert commentary on the match as you are in a sports broadcast.
+    Start your analysis now and engage the audience with your insights.
+    
+    Say: "Hello everyone, I've watched to the match between [Home Team] and [Away Team]..."
+    """
+    llm = GoogleGenerativeAI(model="gemini-pro")
+    input_variables={"match_details": yaml.dump(match_details),
+                    "lineups": yaml.dump(line_ups)}
+    prompt = PromptTemplate.from_template(agent_prompt)
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+    return chain.run(
+        **input_variables
+    )
 
 
 if __name__ == "__main__":
