9310cc685e370a374757315b96dd4df18b11c862
renoaldo <renoaldo@hotmail.com>
2024-12-12 17:40:00 -0300

    Adiciona funções de manipulação no arquivo football.py (parte 1 - Filtragem do elenco inicial)

--- src/football_app/tools/football.py
+++ src/football_app/tools/football.py
@@ -1,4 +1,5 @@
 from statsbombpy import sb  # type: ignore
+import json
 
 def get_match_details_match_id(match_id: int) -> dict:
     """
@@ -11,6 +12,36 @@ def get_match_details_match_id(match_id: int) -> dict:
     """
     raw_data = sb.events(match_id=match_id, fmt="dict")
     return raw_data
+
+
+# Processa o JSON de lineups, extraindo apenas os jogadores titulares.
+def filter_starting_xi(line_ups: str) -> dict:
+    """
+    Filter the starting XI players from the provided lineups.
+    
+    Args:
+        line_ups (str): The JSON string containing the lineups of the teams.
+        
+    Returns:
+    
+    """
+    line_ups_dict = json.loads(line_ups)
+    filter_starting_xi =  {}
+    for team, team_line_up in line_ups_dict.items():
+        filter_starting_xi[team] = []
+        for player in sorted(team_line_up, key= lambda x: x["jersey_number"]):
+            try:
+                positions = player["positions"]["positions"]
+                if positions[0].get("start_reason") == "Starting XI":
+                    filter_starting_xi[team].append({
+                        "player": player["player_name"],
+                        "position": positions[0].get('position'),
+                        "jersey_number": player["jersey_number"]
+                    })
+            except (KeyError, IndexError):
+                continue
+    return filter_starting_xi
+
 
 
 if __name__ == "__main__":
