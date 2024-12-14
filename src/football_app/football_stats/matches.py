fc7a0e9c6660970e4243ab03f0d7b9199fa6ccfa
renoaldo <renoaldo@hotmail.com>
2024-12-14 10:18:56 -0300

    Cria função para retornar eventos (get_events)

--- src/football_app/football_stats/matches.py
+++ src/football_app/football_stats/matches.py
@@ -2,6 +2,7 @@ from statsbombpy import sb
 import json
 from copy import copy
 import pandas as pd
+import numpy as np
 
 
 # Retorna uma string JSON formatada de um DataFrame
@@ -19,3 +20,12 @@ def get_lineups(match_id: int) -> str:
             df[field] = df[field].apply(lambda v: {field: v})
             data_final[key] = df.to_dict(orient='records')
     return to_json(data_final)
+
+def get_events(match_id: int) -> str:
+    events = sb.events(match_id=match_id, split=True, flatten_attrs=False)
+    full_events = pd.concat([v for _, v in events.items()])
+    return to_json([
+        {k: v for k, v in event.items() if v is not np.nan} 
+        for event in full_events.sort_values(by="minute").to_dict(orient='records')
+    ])
+
